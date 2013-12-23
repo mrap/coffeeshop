3d5579b1ab25b1aa9855c7e8a7527f76faa1edb5
Michael Rapadas <mike@mrap.me>
2013-12-23 14:28:43 -0800

    A User must have a unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User
 
   belongs_to :group, inverse_of: :members
   has_and_belongs_to_many :tags
+
+  field :username, type: String
+  validates :username, presence: true, uniqueness: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:username) { |n| "user#{n}" }
   end
 end
