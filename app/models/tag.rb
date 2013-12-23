9eb3e42a78f72e0eb92721103b260575e20584a7
Michael Rapadas <mike@mrap.me>
2013-12-23 14:33:07 -0800

    A Tag must have a name

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,5 +5,6 @@ class Tag
   has_and_belongs_to_many :users
   field :name
 
+  validates_presence_of :name
   validates_uniqueness_of :name, scope: :group
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,5 +4,6 @@ describe Tag do
   it { should belong_to :group }
   it { should have_and_belong_to_many :users }
   it { should have_field :name }
+  it { should validate_presence_of :name }
   it { should validate_uniqueness_of(:name).scoped_to(:group) }
 end
