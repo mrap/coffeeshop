89a823928af15573b18f7d0bdfac92a02b931f5c
Michael Rapadas <mike@mrap.me>
2013-12-31 14:37:55 -0800

    Corrects incomplete :guest_user factory

--- spec/factories/guest_users.rb
+++ spec/factories/guest_users.rb
@@ -1,6 +1,6 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :guest_user do
+  factory :guest_user, class: GuestUser, parent: :user do
   end
 end
