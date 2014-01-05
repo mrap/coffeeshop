beddb7b3d1d6fc809498d80a747f7aae02179d01
Michael Rapadas <mike@mrap.me>
2014-01-05 04:26:46 -0800

    Messages have relative timestamps.
    "less than a minute"
    "about 2 hours"
    
    Notes:
    * Author font is smaller and has a dark blue color.
      * Visually separates from the message content itself.
    * Timestamp placed at the far right edge.

--- spec/features/messaging_on_the_homepage_spec.rb
+++ spec/features/messaging_on_the_homepage_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 
 feature "Users message each other on the front page" do
   background { @message = create(:message) }
+
   scenario "when visiting the home page" do
     visit '/'
     page.should have_content @message.content
   end
+
+  scenario "each message has a relative timestamp" do
+    visit '/'
+    page.should have_content "less than a minute"
+  end
+
 end
