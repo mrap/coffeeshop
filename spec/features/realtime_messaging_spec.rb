a5da8b95aa82e371faafb9009c50e24127520be8
Michael Rapadas <mike@mrap.me>
2014-01-07 09:18:32 -0800

    Fix: Only clears user's textfield if she is the one that posted.
    Before this, all user textfields would clear every time a new message
    was posted.

--- spec/features/realtime_messaging_spec.rb
+++ spec/features/realtime_messaging_spec.rb
@@ -4,25 +4,37 @@ feature "Realtime messages between users", js: true do
   given(:me)         { create(:user) }
   given(:other_user) { create(:user) }
 
-  context "when I visit the home page" do
-    background do
-      in_browser(:mine) do
-        visit '/'
-      end
-      in_browser(:theirs) do
-        visit '/'
-      end
+  background do
+    in_browser(:mine) do
+      visit '/'
     end
-
-    scenario "I can see updates from other users in realtime" do
-      in_browser(:theirs) do
-        fill_in I18n.t('new_message_prompt'), with: "Hello!"
-        click_on I18n.t 'new_message_button'
-      end
-      in_browser(:mine) do
-        find('.message-list').should have_content "Hello!"
-      end
+    in_browser(:theirs) do
+      visit '/'
     end
   end
+
+  scenario "I can see updates from other users in realtime" do
+    in_browser(:theirs) do
+      fill_in I18n.t('new_message_prompt'), with: "Hello!"
+      click_on I18n.t 'new_message_button'
+    end
+    in_browser(:mine) do
+      find('.message-list').should have_content "Hello!"
+    end
+  end
+
+  scenario "when my text box is not empty and another user posts a message" do
+    in_browser(:mine) do
+      fill_in 'Content', with: "Incomplete message"
+    end
+    in_browser(:theirs) do
+      fill_in I18n.t('new_message_prompt'), with: "Hello!"
+      click_on I18n.t 'new_message_button'
+    end
+    in_browser(:mine) do
+      find_field('Content').value.should eq "Incomplete message"
+    end
+  end
+
 end
 
